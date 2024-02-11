758874f374dc122c41229cd3d34fe29357cc5f40
Jesper Ottosen <10980509+jesperottosen@users.noreply.github.com>
2024-02-11 11:08:22 +0100

    notify
    minor renames
    minor code changes
    added mod action

--- LUA/ERFTW_ExploreArtifacts.lua
+++ LUA/ERFTW_ExploreArtifacts.lua
@@ -8,19 +8,30 @@ print("Loaded OK")
 local iPrereqTech = GameInfoTypes.TECH_ARCHAEOLOGY
 local iArtifactRess = GameInfoTypes.RESOURCE_ARTIFACTS
 local iArtifactHidden = GameInfoTypes.RESOURCE_HIDDEN_ARTIFACTS
-local sReturntext = ""
+local sReturntext = "hello mum"
 
 --------------------------------------------------------------
 function doExploreArtifacts(iPlayer,iUnit,iX,iY)
+	-- Assert --
 	local pPlot = Map.GetPlot(iX,iY)
 	if not pPlot then return end
 	
 	local pResource = pPlot:GetResourceType(Game.GetActiveTeam())
 	if (pResource ~= iArtifactRess) and (pResource~= iArtifactHidden) then return end
 
+	
 	print("Plot OK, Ressource OK, Unit ??")
+	-- Act --
+
+	local pPlayer = Players[iPlayer]
 
 
+	-- Notify --
+	print("sreturn: "..sReturntext)
+		local notifyType = GameInfoTypes.NOTIFICATION_GOODY
+		local details = "(ERFTW) A goody bonus: "..sReturntext
+		print("details: "..details)
+	pPlayer:AddNotification(notifyType, sReturntext, details, iX, iY)
 end
 --------------------------------------------------------------
 
